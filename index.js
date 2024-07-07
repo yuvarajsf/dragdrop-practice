abe5f853243efd6fa739544812fa2607ac54eab6
yuvarajsf <yuvaraj.anbazhagan@syncfusion.com>
2024-07-07 23:20:57 +0530

    Serialize components on JSON and re-render items.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function drop(event) {
-    debugger
+window.addEventListener('load', onLoad);
+
+let designerData = [];
+let APIHost = 'http://localhost:5125';
+
+
+async function drop(event) {
     event.preventDefault();
     var currentItem = event.dataTransfer.getData('text');
     var item = document.getElementById(currentItem);
 
-    //prevent exisiting item drag drop issue.
-    var target = event.target.querySelectorAll('#' + currentItem)
+    // Prevent existing item drag drop issue.
+    var target = event.target.querySelectorAll('#' + currentItem);
     if (target.length > 0) {
         item.style.backgroundColor = 'black';
         return;
     }
 
-    document.cookie = 'leftOrRight=' + event.target.className;
-    var exisitingItem = getCookie('itemId');
-    if (!isNullOrUndefindedOrEmpty(exisitingItem)) {
-        document.cookie = 'itemId=' + exisitingItem + ',' + currentItem;
-    } else {
-        document.cookie = 'itemId=' + currentItem;
-    }
+    await updateDesign(item, event);
+
     item.style.backgroundColor = 'black';
-    var target = event.target;
+    target = event.target;
     target.appendChild(item);
 }
 
@@ -32,45 +32,51 @@ function onDrag(event) {
     event.target.style.backgroundColor = 'yellow';
 }
 
-
-function getComponent(id, text) {
+function getRectComponent(id, text, className) {
     var element = document.createElement('div');
     element.id = id;
     element.innerHTML = text;
-    element.className = 'drag';
+    element.className = className;
     element.draggable = true;
+    element.setAttribute('data-name', 'rectangle');
     element.ondragstart = onDrag;
     return element;
 }
 
-
-// reload state maintain process
-var leftContainer = document.getElementById('left');
-var rightContainer = document.getElementById('right');
-
-var cookies = getCookie('leftOrRight');
-
-for (i = 0; i < 3; i++) {
-    var commonClass = 'box';
-    var itemIds = getCookie('itemId');
-    if (!isNullOrUndefindedOrEmpty(itemIds)) {
-        var itemIdsArr = itemIds.split(',');
-        if (itemIdsArr.includes(commonClass + i)) {
-            if (isRightContainer(cookies)) {
-                rightContainer.appendChild(getComponent(commonClass + i, 'Box ' + i));
-            }
-        } else {
-            leftContainer.appendChild(getComponent(commonClass + i, 'Box ' + i));
-        }
-    } else {
-        leftContainer.appendChild(getComponent(commonClass + i, 'Box ' + i));
-    }
-
+function getCircleComponent(id, text, className) {
+    var element = document.createElement('div');
+    element.id = id;
+    element.innerHTML = text;
+    element.className = className;
+    element.draggable = true;
+    element.setAttribute('data-name', 'circle');
+    element.ondragstart = onDrag;
+    return element;
 }
 
-// ______________________________________________________________________
-// add ondragstart event for all the elements using foreach
-document.querySelectorAll('.drag').forEach(function (item) {
+async function onLoad() {
+    // Create default widget panel items:
+    var widgetContainer = document.getElementById('widget-container');
+    var rect = getRectComponent(uuidv4(), 'Rectangle', 'widget');
+    var circle = getCircleComponent(uuidv4(), 'Circle', 'widget');
+    widgetContainer.appendChild(rect);
+    widgetContainer.appendChild(circle);
+
+    // Reload state maintain process
+    try {
+        const response = await fetch(APIHost + '/getdata');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        renderWidget(data);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+// Add ondragstart event for all the elements using forEach
+document.querySelectorAll('.widget').forEach(function (item) {
     item.ondragstart = onDrag;
 });
 
@@ -84,30 +90,98 @@ document.querySelectorAll('.right').forEach(function (item) {
     item.ondrop = drop;
 });
 
-function isRightContainer() {
-    return cookies === 'right';
-}
-
-
-function isNullOrUndefindedOrEmpty(value) {
+function isNullOrUndefinedOrEmpty(value) {
     return value === null || value === undefined || value === '';
 }
 
+function renderWidget(data) {
+    var leftContainer = document.getElementById('left');
+    var rightContainer = document.getElementById('right');
 
+    data.forEach(function (item, index) {
+        let currentWidget = '';
+        if (item.item === 'rectangle') {
+            currentWidget = getRectComponent(item.id, item.name, 'widget' + index);
+        } else if (item.item === 'circle') {
+            currentWidget = getCircleComponent(item.id, item.name, 'widget' + index);
+        }
+        if (item.position === 'right') {
+            rightContainer.appendChild(currentWidget);
+        } else {
+            leftContainer.appendChild(currentWidget);
+        }
+    });
+}
 
+// Serialize the component position data
+async function updateDesign(item, event) {
+    var widgetName = item.getAttribute('data-name');
+    var widgetId = item.id;
+    var widgetText = item.innerHTML;
+    var widgetPosition = event.target.className;
+    var widgetData = {
+        item: widgetName,
+        id: widgetId,
+        name: widgetText,
+        position: widgetPosition
+    };
 
+    await checkAndUpdateDesignerData();
+    checkAndUpdateDataForPost(widgetData, designerData);
 
+    try {
+        const response = await fetch(APIHost + '/update', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(designerData)
+        });
+        const data = await response.json();
+        if (data === true) {
+            console.log('Data updated successfully');
+            designerData = [];
+        }
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
 
+// Utility
 
-// utility
-function getCookie(name) {
-    // Create a regular expression to find the cookie by name
-    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    if (match) {
-        // Return the value of the cookie
-        return match[2];
-    } else {
-        // Return null if the cookie is not found
-        return null;
+// Generate Random Guid
+function uuidv4() {
+    return 'widget-xxxx-xxxx-4xxx-yxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+// Check and update the data
+function checkAndUpdateDataForPost(widgetData, designerData) {
+    var isExist = false;
+    designerData.forEach(function (item) {
+        if (item.id === widgetData.id) {
+            item.position = widgetData.position;
+            isExist = true;
+        }
+    });
+
+    if (!isExist) {
+        designerData.push(widgetData);
+    }
+}
+
+async function checkAndUpdateDesignerData() {
+    if (designerData.length === 0) {
+        try {
+            const response = await fetch(APIHost + '/getdata');
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            designerData = await response.json();
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
     }
 }
